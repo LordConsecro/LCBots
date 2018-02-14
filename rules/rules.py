9e56a5df74377c33a0be5754944e0e3e87acb23c
LordConsecro <gosimmonsd@gmail.com>
2018-02-14 15:44:40 -0500

    update

--- rules/rules.py
+++ rules/rules.py
@@ -13,11 +13,7 @@ class Rules:
         """This does stuff!"""
 
         #Your code will go here
-		rules = ("RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators.\n \
-		 RULE 2: Do NOT mass mention Lord Consecro or any staff member. You will receive a mute.\n \
-		 RULE 3: Be respectful to staff member and supporters. Listen to them and follow their rules.\n \
-		 RULE 4: Use the channels for what they are for. That means actually following what the channel is for.\n \
-		 RULE 5: Advertising you're server or any other server is forbidden!")
+		rules = ("RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators.\nRULE 2: Do NOT mass mention Lord Consecro or any staff member. You will receive a mute.\nRULE 3: Be respectful to staff member and supporters. Listen to them and follow their rules.\nRULE 4: Use the channels for what they are for. That means actually following what the channel is for.\nRULE 5: Advertising you're server or any other server is forbidden!")
 		
 		await self.bot.say(box(rules))
 
