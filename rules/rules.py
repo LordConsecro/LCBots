c762bc599b0b112251652f92ffd8d844ecc91467
LordConsecro <gosimmonsd@gmail.com>
2018-02-14 16:29:25 -0500

    update

--- rules/rules.py
+++ rules/rules.py
@@ -3,19 +3,17 @@ from discord.ext import commands
 from cogs.utils.chat_formatting import box
 
 class Rules:
-    """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
-    async def rules(self, ctx):
+    @commands.command()
+    async def rules(self):
         """This does stuff!"""
 
-        #Your code will go here
 		msg = "RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators.\n\n"
 		
-		await self.bot.say(box(msg))
+		await self.bot.say(msg)
 
 
 def setup(bot):
