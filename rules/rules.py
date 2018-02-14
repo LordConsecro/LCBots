9f598ccc2ef0ece38a3a55f8e1a8e43373059846
LordConsecro <gosimmonsd@gmail.com>
2018-02-14 15:13:56 -0500

    d

--- rules/rules.py
+++ rules/rules.py
@@ -1,12 +1,13 @@
+import discord
 from discord.ext import commands
 
-rules = {
+rules = (
     "Don't advertise any other mods from Nexus or Bethesda.net here. It's annoying to brag about other mod creators.",
 	"Do NOT mass mention Lord Consecro or any staff member. You will receive a mute.",
 	"Be respectful to staff member and supporters. Listen to them and follow their rules.",
 	"Use the channels for what they are for. That means actually following what the channel is for.",
 	"Advertising you're server or any other server is forbidden! If you are caught, you will receive a mute."
-}
+)
 
 
 class Rules:
