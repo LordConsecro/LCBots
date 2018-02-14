01ce28a8bd2020a58198f2fd7d12f4190d2213bd
LordConsecro <gosimmonsd@gmail.com>
2018-02-14 16:53:27 -0500

    update

--- rules/rules.py
+++ rules/rules.py
@@ -5,14 +5,14 @@ class Rules:
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(pass_context=True)
-    async def rules(self, ctx):
-        """This does stuff!"""
-
-		msg = "RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators."
 		
-		await self.bot.say(msg)
+		self.msg = "RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators."
+
+    @commands.command(name="rules", aliases=["RULES", "Rules"])
+    async def rules(self):
+        """This does stuff!"""
+		
+		await self.bot.say(self.msg)
 
 
 def setup(bot):
