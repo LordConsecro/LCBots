cd0b091f77fbab592881cd3192e3893364a3db47
LordConsecro <gosimmonsd@gmail.com>
2018-02-14 15:23:43 -0500

    dw

--- rules/rules.py
+++ rules/rules.py
@@ -8,11 +8,7 @@ class Rules:
     @commands.command()
     async def rules(self):
         """Rules"""
-		await self.bot.say("RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators.")
-		await self.bot.say("RULE 2: Do NOT mass mention Lord Consecro or any staff member. You will receive a mute.")
-		await self.bot.say("RULE 3: Be respectful to staff member and supporters. Listen to them and follow their rules.")
-		await self.bot.say("RULE 4: Use the channels for what they are for. That means actually following what the channel is for.")
-		await self.bot.say("RULE 5: Advertising you're server or any other server is forbidden!")
+		await self.bot.say("RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators.\nRULE 2: Do NOT mass mention Lord Consecro or any staff member. You will receive a mute.\nRULE 3: Be respectful to staff member and supporters. Listen to them and follow their rules.\nRULE 4: Use the channels for what they are for. That means actually following what the channel is for.\nRULE 5: Advertising you're server or any other server is forbidden!")
 			
 
 def setup(bot):
