96b6458bf427dc37b66978081daacdfaa6b06c61
LordConsecro <gosimmonsd@gmail.com>
2018-02-14 16:25:58 -0500

    update

--- rules/rules.py
+++ rules/rules.py
@@ -8,12 +8,16 @@ class Rules:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def rules(self):
+    @commands.command(pass_context=True)
+    async def rules(self, ctx):
         """This does stuff!"""
 
         #Your code will go here
-		msg = "RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators.\nRULE 2: Do NOT mass mention Lord Consecro or any staff member. You will receive a mute.\nRULE 3: Be respectful to staff member and supporters. Listen to them and follow their rules.\nRULE 4: Use the channels for what they are for. That means actually following what the channel is for.\nRULE 5: Advertising you're server or any other server is forbidden!"
+		msg = "RULE 1: Don't advertise any other mods. It's annoying to brag about other mod creators.\n\n"
+		msg += "RULE 2: Do NOT mass mention Lord Consecro or any staff member. You will receive a mute.\n\n"
+		msg += "RULE 3: Be respectful to staff member and supporters. Listen to them and follow their rules.\n\n"
+		msg += "RULE 4: Use the channels for what they are for. That means actually following what the channel is for.\n\n"
+		msg += "RULE 5: Advertising you're server or any other server is forbidden!"
 		
 		await self.bot.say(box(msg))
 
